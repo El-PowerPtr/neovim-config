99f6b3acb37e9c23a442a16c8c73fca157a4d8ba
El-PowerPtr <moiseselpower@gmail.com>
2024-11-03 03:26:02 -0500

    algunos cambios a telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,12 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>fs', 
+	function()
+		builtin.grep_string({ sreach = vim.fn.input("Search word >> ") } )
+	end, 
+	{ desc = 'Telescope live string search' });
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
 
 return {
     'nvim-telescope/telescope.nvim',
