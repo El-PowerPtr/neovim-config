117a689a87c5815d4195cab35bd928068634f478
El-PowerPtr <moiseselpower@gmail.com>
2024-11-04 02:39:40 -0500

    ya hay treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require "keybidings"
 require "config.lazy"
+vim.cmd.colorscheme "mellow" 
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )  
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"} )  
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,19 @@
-local builtin = require('telescope.builtin')
+--Telescope keybidings--------------------------------------------------------------------------------------------------
+local builtin = require "telescope.builtin"
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set('n', '<leader>fs', 
-	function()
+vim.keymap.set('n', '<leader>of', builtin.oldfiles, { desc = 'Telescope find old files' })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fs', function()
 		builtin.grep_string({ sreach = vim.fn.input("Search word >> ") } )
 	end, 
 	{ desc = 'Telescope live string search' });
-vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
-
+	
+------------------------------------------------------------------------------------------------------------------------
 return {
     'nvim-telescope/telescope.nvim',
 }
